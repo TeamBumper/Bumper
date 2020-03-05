ebfa41eccf194494ce01437e3db14ed9da4350c0
alans2021 <als432@pitt.edu>
2020-03-05 13:38:04 -0500

    After OAuth done by firebase, renders home page

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import os
 from flask import Flask, request, session, url_for, redirect, render_template, abort, g, flash, _app_ctx_stack
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'catering.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  #here to silence deprecation warning
-app.config['SECRET_KEY'] = 'leak'
+
 
 @app.route('/')
 def init():
     return render_template('welcome/welcome.html')
+
+
+@app.route('/home')
+def home_screen():
+    print("HERE")
+    return render_template('home/home.html')
