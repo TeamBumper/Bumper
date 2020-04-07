3800d8238cff0d5e0826785db22848c1b37abe83
Marcus Beacon <marcusbeacon@gmail.com>
2020-04-07 16:20:39 -0400

    Added cards

--- static/home/home_script.js
+++ static/home/home_script.js
@@ -1,4 +1,6 @@
 getCars();
+var card_cont = document.getElementById('card-cont');
+var all_cars;
 
 function getCars(){
     endpoint = '/search';
@@ -7,7 +9,60 @@ function getCars(){
 }
 
 function handleCars(response) {
-    console.log(response.responseText);
+    var object = JSON.parse(response.responseText);
+    var j_object = JSON.parse(object);    
+    allCars = j_object["listings"];
+    allCars.forEach(car => {
+        buildCard(car);
+    });
+}
+
+function buildCard(car) {
+    // Card structure
+    var new_card = document.createElement("div");
+    new_card.className = "card";
+    new_card.id = car['vin'];
+    var card_top = document.createElement("div");
+    card_top.className = "card__top purple";
+    var card_bot = document.createElement("div");
+    card_bot.className = "card__btm";
+    
+    // Card top 
+    var card_img = document.createElement("div");
+    card_img.className = "card__img";
+    var img = document.createElement("img");
+    img.id = car['vin'] + "-img-0";
+    img.addEventListener('click', goToNextImage);
+    img.src = car['media']['photo_links'][0];
+    card_img.appendChild(img);
+
+    var card_name = document.createElement("div");
+    card_name.className = "card__name";
+    card_name.innerText = car['heading'];
+
+    card_top.appendChild(card_img);
+    card_top.appendChild(card_name);
+
+    new_card.appendChild(card_top);
+    new_card.appendChild(card_bot);
+
+    card_cont.appendChild(new_card);
+}
+
+function goToNextImage(e) {
+    var id = (e.target.id).split("-");
+    var vin = id[0];
+    var index = parseInt(id[2]);
+    
+    var thisCar;
+    allCars.forEach(car => {
+        if (car['vin'] == vin) thisCar = car;
+    });
+
+    e.target.src = thisCar['media']['photo_links'][++index];
+
+    if(index >= 4) index = 0;
+    e.target.id = vin + "-img-" + index;
 }
 
 /* AJAX Boilerplate */
