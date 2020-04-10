449286ca07741d89f00da641fedd0e08f9a92cbd
bhellested <55294326+bhellested@users.noreply.github.com>
2020-04-10 17:33:55 -0400

    fixed swiping bug

--- marketcheck.py
+++ marketcheck.py
@@ -1,47 +1,50 @@
 import requests
 import json
 
-api_key = "BfUQMAGwMnOY1Etlduya1MpBLS8IcD6J"
+api_key = "SEo7nNrTUsuLuxt6CEjVsuGbokhrmj1I"
 
-class SearchSystem():    
-    
+
+class SearchSystem():
+
     def __init__(self):
-        self.filters = dict()                
-    
+        self.filters = dict()
+
     def setFilter(self, param, value):
-        self.filters[param] = value 
+        self.filters[param] = value
 
     def search(self):
-        #build the url
+        # build the url
         print("Searching")
-        query = "http://api.marketcheck.com/v2/search/car/active?api_key={0}".format(api_key)
-        for param in self.filters:                        
+        query = "http://api.marketcheck.com/v2/search/car/active?api_key={0}".format(
+            api_key)
+        for param in self.filters:
             filter = "&{0}={1}".format(param, self.filters[param])
-            query += filter            
-            
+            query += filter
+
         payload = {}
         headers = {
             'Host': 'marketcheck-prod.apigee.net',
             'content-Type': 'application/json'
         }
-    
-        response = requests.request("GET", query, headers=headers, data = payload)
+
+        response = requests.request(
+            "GET", query, headers=headers, data=payload)
         # print(response.text.encode('utf8'))
         # response['listings']
         # for each listing print listing[vin]
 
-        json_object = json.loads(response.text.encode('utf8'))        
-        #print(json_object["num_found"])
+        json_object = json.loads(response.text.encode('utf8'))
+        # print(json_object["num_found"])
         # print(json.dumps(json_object, indent=1))
-        
-        return json_object        
+
+        return json_object
         # return json response
 
     def debug_print(self, response):
         for vehicle in response["listings"]:
             print(vehicle["vin"])
-        
-            
+
+
 if __name__ == '__main__':
     test = SearchSystem()
     test.setFilter('make', 'Chevrolet')
